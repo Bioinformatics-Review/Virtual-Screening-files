d9927ccf2f6457d3ad5d3f78cd065bfe70be7aea
Bioinformatics-Review <58262772+Bioinformatics-Review@users.noreply.github.com>
2020-04-10 20:06:10 +0530

    Create vina_vs_win.pl

---
+++ vina_vs_win.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+print"Enter filename of ligands list:\t";
+$ligfile=<STDIN>;
+chomp $ligfile;
+open (FH,$ligfile)||die "Cannot open file\n";
+@arr_file=<FH>;
+
+for($i=0;$i<@arr_file;$i++)
+{
+print"@arr_file[$i]\n";
+@name=split(/\./,@arr_file[$i]);
+}
+for($i=0;$i<@arr_file;$i++)
+{
+	chomp @arr_file[$i];
+	print"@arr_file[$i]\n";
+	system("vina.exe --config conf.txt --ligand @arr_file[$i] --log @arr_file[$i]_log.log");
+}
